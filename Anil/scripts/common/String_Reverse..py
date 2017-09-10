06c5a9b2577280c7bb33beb99530d72285f11ebc
baba@85 <anilbhuma999@gmail.com>
2017-09-10 14:24:03 +0530

    Create dir anil

---
+++ Anil/scripts/common/String_Reverse..py
@@ -0,0 +1,9 @@
+# 1 st way
+str1=raw_input("Enter the String :")
+reverse_str=str1[::-1]
+print reverse_str
+# 2nd way
+str1=raw_input("enter the string:")
+p=str1.split()
+reverse_str= (" ".join(p))[::-1]
+print reverse_str
